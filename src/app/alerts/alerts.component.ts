a3379613cb523c4d008eba38516033553b88e387
Fabio Galante <fabiogalantemans@gmail.com>
2017-04-18 15:14:34 -0300

    data binding

--- src/app/alerts/alerts.component.ts
+++ src/app/alerts/alerts.component.ts
@@ -3,21 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.component.html',
-  styleUrls: ['./alerts.component.css']
+  // styleUrls: ['./alerts.component.css']
+  styles: [
+    `
+   .highlight{
+    background-color: yellow;
+    font-weight: bold;
+}
+   `
+  ]
 })
 export class AlertsComponent implements OnInit {
 
 
-valorAtual: string;
+  valorAtual: string;
+  valorSalvo: string;
+  isMouseOver: boolean = false;
 
 
   botaoClicado() {
     alert('Botão clicado')
   }
 
-
   onKeyUp(evento: KeyboardEvent) {
-     this.valorAtual = ((<HTMLInputElement>evento.target).value);    
+    this.valorAtual = ((<HTMLInputElement>evento.target).value);
+  }
+
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver   
+    console.log(this.isMouseOver);
+  }
+
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
   }
 
   constructor() { }
